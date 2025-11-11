6fa03acb77851e5d3cf358b9c01e255b3c5d9b05
amiraadum <ava8264@nyu.edu>
2025-11-11 15:05:01 -0500

    Change Flask port to 3000 to avoid AirPlay conflict

--- web-app/app.py
+++ web-app/app.py
@@ -24,4 +24,4 @@ def create_app():
 
 if __name__ == "__main__":
     flask_app = create_app()
-    flask_app.run(host="0.0.0.0", port=5000, debug=True)
+    flask_app.run(host="0.0.0.0", port=3000, debug=True)
