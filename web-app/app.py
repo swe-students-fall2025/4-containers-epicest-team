81c8f5176d54d2aa6e6e2afbba53fa63fd48ff5c
amiraadum <ava8264@nyu.edu>
2025-11-10 22:56:37 -0500

    Add frontend skeleton: base, index, dashboard templates and style.css

---
+++ web-app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+
+def create_app():
+    #Application factory for password guess web app
+    app = Flask(__name__)
+
+    @app.route("/")
+    def index():
+        return render_template("index.html")
+
+    @app.route("/dashboard")
+    def dashboard():
+        return render_template("dashboard.html")
+
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(host="0.0.0.0", port=5000, debug=True)
